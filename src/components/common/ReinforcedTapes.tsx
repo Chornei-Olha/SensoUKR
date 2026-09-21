395abaa17ceb99172f2907f2c0d62cd208de3917
Olha Chornei <Chornei8053@gmail.com>
2026-09-21 23:09:08 +0300

    Зміна розміру контейнеру для фото

--- src/components/common/ReinforcedTapes.tsx
+++ src/components/common/ReinforcedTapes.tsx
@@ -41,7 +41,7 @@ export default function ReinforcedTapes() {
               className="group block border-b border-gray-200 transition-all duration-300 hover:bg-gray-50 hover:shadow-sm"
             >
               <div className="flex items-center gap-5 sm:gap-8 py-6 px-2 sm:px-4">
-                <div className="relative w-[110px] sm:w-[150px] aspect-[4/5] shrink-0 overflow-hidden rounded-lg bg-gray-100">
+                <div className="relative w-[165px] sm:w-[225px] aspect-[3/2] shrink-0 overflow-hidden rounded-lg bg-gray-100">
                   {' '}
                   <Image
                     src={product.image}
